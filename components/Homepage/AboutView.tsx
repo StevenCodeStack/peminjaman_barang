04fd515846bcf01f21279e9cdf64b0a648a7d65b
StevenCodeStack <steven.codestack@gmail.com>
2025-05-05 19:25:33 +0800

    AboutView Component Done

--- components/Homepage/AboutView.tsx
+++ components/Homepage/AboutView.tsx
@@ -1,9 +1,46 @@
+import Image from "next/image";
 import React from "react";
+import AboutImg from "../../public/images/about_img.png";
 
 const AboutView = () => {
   return (
-    <div className="px-10 md:px-20 xl:px-30 h-[90vh] bg-black w-full">
-      AboutView
+    <div className="paddingLayout flex flex-col lg:flex-row justify-center items-center md:gap-10 bg-black">
+      <div className="relative flex-grow aspect-[4/3] h-80 lg:aspect-auto lg:h-[500px]">
+        <Image src={AboutImg} fill alt="asd123" className="object-contain" />
+      </div>
+      <div className="flex-grow max-h-1/2 lg:max-w-1/2 mt-5">
+        <h1 className="font-rubik text-4xl text-white">
+          ABOUT<span className="text-primary">US</span>
+        </h1>
+        <p className="text-[#9AA1A3] mt-1">
+          &quot;ShareIT is a reliable borrowing platform that makes accessing
+          electronics easy and hassle-free. We ensure a smooth borrowing
+          experience with a focus on security and convenience&quot;
+        </p>
+
+        <div className="grid grid-cols-2 grid-rows-2 gap-4 text-white py-5">
+          <div className="flex items-center gap-2">
+            <h1 className="text-7xl font-rubik">1</h1>
+            <p className="italic font-light text-xl">Simple and Secure</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <h1 className="text-7xl font-rubik">2</h1>
+            <p className="italic font-light text-xl">
+              Reliable for all Members
+            </p>
+          </div>
+          <div className="flex items-center gap-2">
+            <h1 className="text-7xl font-rubik">3</h1>
+            <p className="italic font-light text-xl">Fast and Easy Process</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <h1 className="text-7xl font-rubik">4</h1>
+            <p className="italic font-light text-xl">
+              Borrow only what you need when you need it
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Homepage/HomeView.tsx
+++ components/Homepage/HomeView.tsx
@@ -4,8 +4,8 @@ import HomeImg from "../../public/images/homepage_img.png";
 
 const HomeView = () => {
   return (
-    <div className="flex flex-col-reverse lg:flex-row justify-center items-center h-[80vh] md:gap-5 lg:gap-0 px-10 md:px-20 xl:px-30">
-      <div className="max-w-1/2 flex flex-col items-center lg:items-start text-center lg:text-start">
+    <div className="paddingLayout flex flex-col-reverse lg:flex-row justify-center items-center h-[80vh] md:gap-5 lg:gap-0">
+      <div className="max-w-15/16 lg:max-w-[60%] xl:max-w-[50%] flex flex-col items-center lg:items-start text-center lg:text-start">
         <h1 className="font-rubik text-[40px] md:text-5xl leading-11 md:leading-12 text-black text-shadow-lg">
           MAKE YOUR LIFE <span className="text-primary-hover">EASIER</span> WITH
           SHARE
@@ -14,7 +14,7 @@ const HomeView = () => {
         <p className="text-black font-semibold text-lg w-7/12">
           Why buy when you can borrow? Got the tech you need today!
         </p>
-        <div className="flex gap-10 mt-5 text-xl">
+        <div className="flex gap-10 my-5 text-xl">
           <button className="px-7 py-3 border-2 border-black">
             Get Started
           </button>
